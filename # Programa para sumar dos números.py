8bcebe0e9d26e360fafc983c8267e979214dd43b
JoseP23J <Jose.pachecovergara23@gmail.com>
2025-01-29 08:11:35 -0500

    Add files via upload

---
+++ # Programa para sumar dos números.py
@@ -0,0 +1,11 @@
+# Programa para sumar dos números
+
+# Solicitar al usuario que ingrese dos números
+numero1 = float(input("Ingresa el primer número: "))
+numero2 = float(input("Ingresa el segundo número: "))
+
+# Sumar los dos números
+resultado = numero1 + numero2
+
+# Mostrar el resultado
+print("La suma de", numero1, "y", numero2, "es:", resultado)
